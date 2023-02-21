f162e571c5039571750dcde250c3cadd70eda2ec
davidgreenfield <dgreenfi@gmail.com>
2023-02-21 12:54:17 -0500

    Initial commit

---
+++ ServiceBot.py
@@ -0,0 +1,94 @@
+from flask import Flask, request,render_template
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+from gpt_index import GPTSimpleVectorIndex,SimpleDirectoryReader
+from gpt_index import LLMPredictor, GPTSimpleVectorIndex, PromptHelper
+from langchain.prompts import BasePromptTemplate
+from pydantic import BaseModel, validator
+
+
+class StavrosPromptTemplate(BasePromptTemplate, BaseModel):
+    """ A custom prompt template that takes in the function name as input, and formats the prompt template to provide the source code of the function. """
+
+    @validator("input_variables")
+    def validate_input_variables(cls, v):
+        """ Validate that the input variables are correct. """
+        if "question" not in v:
+            raise ValueError("question must be an input_variable.")
+        return v
+
+    def format(self, **kwargs) -> str:
+        # Get the source code of the function
+
+        # Generate the prompt to be sent to the language model
+        prompt = f"""
+        Answer the question concisely while pretending you are an old Greek grandpa (with 4 grandkids) named Stavros excited to have people experience your Greek culture at Little Greek Fresh Grill.
+        Also include connections from responses to your childhood stories in Mykonos.  Tell people your name if it seems relevant.
+        You can also tell them about your favorite Greek foods (Feta Cheese, Pastitsio and Lamb Gyro), and how you make it. Keep responses to under 4 sentences.
+        
+        Question:{kwargs["question"]}
+        """
+        return prompt
+
+    def _prompt_type(self):
+        return "service-bot"
+
+app = Flask(__name__)
+@app.route('/bot', methods=['POST'])
+#out of the box tutorial from Twilio
+def bot():
+    #how to process message
+    incoming_msg = request.values.get('Body', '').lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+    #opentional logic
+    if 'quote' in incoming_msg:
+        # return a quote
+        r = requests.get('https://api.quotable.io/random')
+        if r.status_code == 200:
+            data = r.json()
+            quote = f'{data["content"]} ({data["author"]})'
+        else:
+            quote = 'I could not retrieve a quote at this time, sorry.'
+        msg.body(quote)
+        responded = True
+    if 'cat' in incoming_msg:
+        # return a cat pic
+        msg.media('https://cataas.com/cat')
+        responded = True
+    if not responded:
+        msg.body('I only know about famous quotes and cats, sorry!')
+    return str(resp)
+@app.route('/ServiceBot', methods=['POST'])
+def Stavros():
+    #process incoming message
+    incoming_msg = request.values.get('Body', '').lower()
+    #create response object
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+    #call index to query
+    StavrosPrompt = StavrosPromptTemplate(input_variables=["question"])
+    prompt = (StavrosPrompt.format(question=incoming_msg))
+    response = index.query(prompt)
+    print(incoming_msg)
+    msg.body(str(response))
+    print(response)
+    responded = True
+    return str(resp)
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html', messages=["hi", "hello"])
+
+if __name__ == '__main__':
+    #load index
+    dotenv_path = Path('CRED.env')
+    load_dotenv(dotenv_path=dotenv_path)
+    os.getenv('OPENAI_API_KEY')
+    index = GPTSimpleVectorIndex.load_from_disk('index.json')
+    #run the app
+    app.run(host="localhost",port=5003,debug=True)
---
+++ indexing.py
@@ -0,0 +1,73 @@
+# This is a sample Python script.
+from dotenv import load_dotenv
+from pathlib import Path
+from gpt_index import GPTSimpleVectorIndex,SimpleDirectoryReader
+from gpt_index import LLMPredictor, GPTSimpleVectorIndex, PromptHelper
+import os
+import inspect
+from ServiceBot import StavrosPromptTemplate
+from gpt_index import (
+    GPTKeywordTableIndex,
+    SimpleDirectoryReader,
+    LLMPredictor
+)
+from langchain import Cohere
+
+def get_source_code(function_name):
+    # Get the source code of the function
+    return inspect.getsource(function_name)
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def get_local_auth(envpath,v):
+    dotenv_path = Path(envpath)
+    load_dotenv(dotenv_path=dotenv_path)
+    return os.getenv(v)
+
+def generate_index(pages, max_input_size=4096,num_output=256,max_chunk_overlap=20):
+    # define LLM
+
+    #failed attempt to use Cohere - embedding not recognizing Cohorer LLM and still calling OpenAI
+    #cohere = Cohere(temperature=.5, k=5)
+    #llm_predictor = LLMPredictor(llm=cohere)
+    #get_local_auth('CRED.env', 'COHERE_API_KEY')
+    LLMPredictor(llm=Cohere(temperature=.5, k=5))
+
+    # define prompt helper
+    prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
+    documents = SimpleDirectoryReader('data').load_data()
+    index = GPTSimpleVectorIndex(
+        documents, prompt_helper=prompt_helper
+    )
+    index.save_to_disk('index.json')
+    return index
+
+def create_index(folder):
+     # get embeddings for confluence data
+    get_local_auth('CRED.env', 'OPENAI_API_KEY')
+    #get_local_auth('CRED.env', 'COHERE_API_KEY')
+    # construct index
+    index = generate_index(folder)
+
+def query_index(prompt):
+    AI_token = get_local_auth('CRED.env', 'OPENAI_API_KEY')
+    index = GPTSimpleVectorIndex.load_from_disk('index.json')
+    response = index.query(prompt)
+    print(response)
+
+
+
+def main():
+    # Use a breakpoint in the code line below to debug your script.
+    print('hi')  # Press ⌘F8 to toggle the breakpoint.
+    create_index('data')
+    sms = "What should i get for a healthy dinner?"
+    StavrosPrompt = StavrosPromptTemplate(input_variables=["question"])
+    prompt=(StavrosPrompt.format(question=sms))
+    query_index(prompt)
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
